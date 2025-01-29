3b49a8c9dd7b8d846a0d628e1be53181eea87629
marcovela <marcorvelak@gmail.com>
2025-01-29 18:38:57 -0500

    Added resume to page and fixed some animations

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const typingSpeed = 90;
+const typingSpeed = 60;
 
 function typeWriter(text, id, i = 0) {
     if (i < text.length) {
         document.getElementById(id).innerHTML += text.charAt(i);
         i++;
-        setTimeout(() => typeWriter(text, id, i), typingSpeed); // Pass i here
+        setTimeout(() => typeWriter(text, id, i), typingSpeed);
     }
 }
 
 window.onload = function() {
-    typeWriter('Marco Vela-Koentarjo', 'title'); // Assuming the element has id="title"
+    typeWriter('Marco Vela-Koentarjo', 'title');
 };
